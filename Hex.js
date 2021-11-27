70231c98b04feed0fd148aa038b6da57acdb798f
Muhammad Salman <92182319+m-salman004@users.noreply.github.com>
2021-11-26 23:27:10 -0800

    Add const colors

---
+++ Hex.js
@@ -0,0 +1,25 @@
+const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById('btn');
+
+const color = document.querySelector('.color');
+
+btn.addEventListener("click", function (e){
+
+    let hexColor = '#';
+
+    for(let i = 0; i < 6; i++){
+
+        hexColor +=  hex[getRandomNumber()];
+    }
+
+    document.body.style.BackgroundColor = hexColor;
+
+    color.textContent = hexColor;
+
+});
+
+function getRandomNumber() {
+    
+    return Math.floor(Math.random() * hex.length);
+}
